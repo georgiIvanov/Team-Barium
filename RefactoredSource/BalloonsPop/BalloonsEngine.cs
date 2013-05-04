412963f8763bcd16821ad9c2cccefa42968a274d
Georgi <georgi.ivanovtn@gmail.com>
2013-05-04 20:08:27 +0300

    Project fully refactored
    game logic and i/o is separated, methods are now cohesive and testible

---
+++ RefactoredSource/BalloonsPop/BalloonsEngine.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Balloons_Pops_game
+{
+    class BalloonsEngine
+    {
+        int fieldRows, fieldCols, userMoves;
+        int[,] playField;
+        string[,] topFive;
+
+        public BalloonsEngine(int rows, int columns)
+        {
+            playField = GeneratePlayField(rows, columns);
+            fieldRows = rows;
+            fieldCols = columns;
+            userMoves = 0;
+            topFive = new string[5, 2];
+        }
+
+        public int UserMoves
+        {
+            get
+            {
+                return userMoves;
+            }
+            set
+            {
+                this.userMoves = value;
+            }
+        }
+        public int FieldRows
+        {
+            get
+            {
+                return fieldRows;
+            }
+        }
+        public int FieldColumns
+        {
+            get
+            {
+                return fieldCols;
+            }
+        }
+
+        int[,] GeneratePlayField(int rows, int columns)
+        {
+            int[,] playField = new int[rows, columns];
+            Random randNumber = new Random();
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    playField[row, column] = randNumber.Next(1, 5);
+                }
+            }
+            return playField;
+        }
+
+        public bool CheckMoveValidity(string userInput)
+        {
+            return (userInput.Length == 3) &&
+                 (userInput[0] >= '0' && userInput[0] <= '4') &&
+                 (userInput[2] >= '0' && userInput[2] <= '9') &&
+                 (userInput[1] == ' ' || userInput[1] == '.' || userInput[1] == ',');
+        }
+
+        public string FieldOutput()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("    ");
+            for (int column = 0; column < fieldCols; column++)
+            {
+                result.Append(column + " ");
+            }
+
+            result.Append("\n   ");
+            for (int column = 0; column < fieldCols * 2 + 1; column++)
+            {
+                result.Append("-");
+            }
+
+            result.AppendLine();
+
+            for (int i = 0; i < fieldRows; i++)
+            {
+                result.Append(i + " | ");
+                for (int j = 0; j < fieldCols; j++)
+                {
+                    if (playField[i, j] == 0)
+                    {
+                        result.Append("  ");
+                        continue;
+                    }
+
+                    result.Append(playField[i, j] + " ");
+                }
+                result.AppendLine("| ");
+            }
+
+            result.Append("   ");
+            for (int column = 0; column < fieldCols * 2 + 1; column++)
+            {
+                result.Append("-");
+            }
+
+            result.AppendLine();
+            return result.ToString();
+        }
+
+        public void CollapseRows()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            for (int col = 0; col < fieldCols; col++)
+            {
+                for (int i = 0; i < fieldRows; i++)
+                {
+                    if (playField[i, col] != 0)
+                    {
+                        stack.Push(playField[i, col]);
+                    }
+                }
+
+                for (int k = fieldRows - 1; k >= 0; k--)
+                {
+                    if (stack.Count > 0)
+                    {
+                        playField[k, col] = stack.Pop();
+                    }
+                    else
+                    {
+                        playField[k, col] = 0;
+                    }
+                }
+            }
+        }
+
+        public bool CheckIfWinning()
+        {
+            for (int i = 0; i < fieldRows; i++)
+            {
+                for (int j = 0; j < fieldCols; j++)
+                {
+                    if (playField[i, j] != 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool TryPopBalloons(int row, int col)
+        {
+            if (playField[row, col] == 0)
+            {
+                return false;
+            }
+
+            int searchedTarget = playField[row, col];
+            playField[row, col] = 0;
+
+            PopRowsAndCols(row, col - 1, searchedTarget, 3);
+            PopRowsAndCols(row, col + 1, searchedTarget, 4);
+            PopRowsAndCols(row - 1, col, searchedTarget, 1);
+            PopRowsAndCols(row + 1, col, searchedTarget, 2);
+            return true;
+        }
+
+        private void PopRowsAndCols(int row, int col, int searchedItem, int direction)
+        {
+            if (row < 0 || row >= fieldRows || col < 0 || col >= fieldCols)
+            {
+                return;
+            }
+
+            if (searchedItem != playField[row, col])
+            {
+                return;
+            }
+
+            playField[row, col] = 0;
+
+            switch (direction)
+            {
+                case 1: PopRowsAndCols(row - 1, col, searchedItem, direction); break;
+                case 2: PopRowsAndCols(row + 1, col, searchedItem, direction); break;
+                case 3: PopRowsAndCols(row, col - 1, searchedItem, direction); break;
+                case 4: PopRowsAndCols(row, col + 1, searchedItem, direction); break;
+                default:
+                    break;
+            }
+        }
+
+        public void RestartGame()
+        {
+            playField = GeneratePlayField(5, 10);
+            userMoves = 0;
+        }
+
+        public string GenerateChart()
+        {
+            StringBuilder result = new StringBuilder();
+            List<ScoreEntry> scores = new List<ScoreEntry>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (topFive[i, 0] == null)
+                {
+                    break;
+                }
+
+                scores.Add(new ScoreEntry(int.Parse(topFive[i, 0]), topFive[i, 1]));
+            }
+
+            scores.Sort();
+            result.AppendLine("---------TOP FIVE CHART-----------");
+            for (int i = 0; i < scores.Count; ++i)
+            {
+                result.AppendFormat("{2}. {0} with {1} moves.\n", scores[i].Name, scores[i].Score, i + 1);
+            }
+            result.AppendLine("----------------------------------");
+
+            return result.ToString();
+        }
+
+        public int ChartPlaceIndex()
+        {
+            int userPlace = -1;
+            for (int i = 0; i < topFive.GetLength(0); i++)
+            {
+                if (topFive[i, 0] == null)
+                {
+                    userPlace = i;
+                    break;
+                }
+            }
+
+            if (userPlace == -1)
+            {
+                int worstScore = int.MinValue;
+                for (int i = 0; i < topFive.GetLength(0); i++)
+                {
+                    int currentScore = int.Parse(topFive[i, 0]);
+                    if (currentScore > worstScore && userMoves < currentScore)
+                    {
+                        worstScore = currentScore;
+                        userPlace = i;
+                    }
+                }
+            }
+
+            return userPlace;
+        }
+
+        public void signIfSkilled(string username, int place)
+        {
+            topFive[place, 0] = userMoves.ToString();
+            topFive[place, 1] = username;
+        }
+    }
+}
--- RefactoredSource/BalloonsPop/Program.cs
+++ RefactoredSource/BalloonsPop/Program.cs
@@ -1,218 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Balloons_Pops_game
 {
-    
-
     class Program
     {
-        static byte[,] GeneratePlayField(byte rows, byte columns)
-        {
-            byte[,] temp = new byte[rows, columns];
-            Random randNumber = new Random();
-            for (byte row = 0; row < rows; row++)
-            {
-                for (byte column = 0; column < columns; column++)
-                {
-                    byte tempByte = (byte)randNumber.Next(1, 5);
-                    temp[row, column] = tempByte;
-                }
-            }
-            return temp;
-        }
-
-        static void PrintField(byte[,] matrix)
-        {
-            Console.Write("    ");
-            for (byte column = 0; column < matrix.GetLongLength(1); column++)
-            {
-                Console.Write(column + " ");
-            }
-
-
-            Console.Write("\n   ");
-            for (byte column = 0; column < matrix.GetLongLength(1) * 2 + 1; column++)
-            {
-                Console.Write("-");
-            }
-
-            Console.WriteLine();
-
-            for (byte i = 0; i < matrix.GetLongLength(0); i++)
-            {
-                Console.Write(i + " | ");
-                for (byte j = 0; j < matrix.GetLongLength(1); j++)
-                {
-                    if (matrix[i, j] == 0)
-                    {
-                        Console.Write("  ");
-                        continue;
-                    }
-
-
-
-                    Console.Write(matrix[i, j] + " ");
-                }
-                Console.Write("| ");
-                Console.WriteLine();
-            }
-
-            Console.Write("   ");
-            for (byte column = 0; column < matrix.GetLongLength(1) * 2 + 1; column++)
-            {
-                Console.Write("-");
-            }
-            Console.WriteLine();
-        }
-
-        static void PopRowsAndCols(byte[,] gameField, int row, int col, int searchedItem, int direction)
-        {
-            if (row < 0 || row >= gameField.GetLength(0) || col < 0 || col >= gameField.GetLength(1))
-            {
-                return;
-            }
-
-            if (searchedItem != gameField[row, col])
-            {
-                return;
-            }
-
-            gameField[row, col] = 0;
-
-            switch (direction)
-            {
-                case 1: PopRowsAndCols(gameField, row - 1, col, searchedItem, direction); break;
-                case 2: PopRowsAndCols(gameField, row + 1, col, searchedItem, direction); break;
-                case 3: PopRowsAndCols(gameField, row, col - 1, searchedItem, direction); break;
-                case 4: PopRowsAndCols(gameField, row, col + 1, searchedItem, direction); break;
-                default:
-                    break;
-            }
-        }
-
-
-        static bool PopBalloons(byte[,] matrixToModify, int row, int col)
-        {
-            if (matrixToModify[row, col] == 0)
-            {
-                return true;
-            }
-            byte searchedTarget = matrixToModify[row, col];
-            matrixToModify[row, col] = 0;
-
-            PopRowsAndCols(matrixToModify, row, col - 1, searchedTarget, 3);
-            PopRowsAndCols(matrixToModify, row, col + 1, searchedTarget, 4);
-            PopRowsAndCols(matrixToModify, row - 1, col, searchedTarget, 1);
-            PopRowsAndCols(matrixToModify, row + 1, col, searchedTarget, 2);
-            return false;
-        }
-
-        static bool CollapseRows(byte[,] matrix)
-        {
-            bool isWinner = true;
-            Stack<byte> stack = new Stack<byte>();
-
-            int rows = matrix.GetLength(0);
-
-            for (int col = 0; col < matrix.GetLength(1); col++)
-            {
-                for (int i = 0; i < rows; i++)
-                {
-                    if (matrix[i, col] != 0)
-                    {
-                        isWinner = false;
-                        stack.Push(matrix[i, col]);
-                    }
-                }
-                for (int k = rows - 1; k >= 0 ; k--)
-                {
-                    if(stack.Count > 0)
-                    {
-                        matrix[k, col] = stack.Pop();
-                    }
-                    else
-                    {
-                        matrix[k, col] = 0;
-                    }
-                }
-            }
-            return isWinner;
-        }
-
-        static void sortAndPrintChartFive(string[,] tableToSort)
-        {
-            List<ScoreEntry> scores = new List<ScoreEntry>();
-
-            for (int i = 0; i < 5; i++)
-            {
-                if (tableToSort[i, 0] == null)
-                {
-                    break;
-                }
-
-                scores.Add(new ScoreEntry(int.Parse(tableToSort[i, 0]), tableToSort[i, 1]));
-            }
-
-            scores.Sort();
-            Console.WriteLine("---------TOP FIVE CHART-----------");
-            for (int i = 0; i < scores.Count; ++i)
-            {
-                ScoreEntry slot = scores[i];
-                Console.WriteLine("{2}.   {0} with {1} moves.", slot.Name, slot.Score, i + 1);
-            }
-            Console.WriteLine("----------------------------------");
-
-
-        }
-
-        static bool signIfSkilled(string[,] Chart, int points)
-        {
-            bool Skilled = false;
-            int worstMoves = 0;
-            int worstMovesChartPosition = 0;
-            for (int i = 0; i < 5; i++)
-            {
-                if (Chart[i, 0] == null)
-                {
-                    Console.WriteLine("Type in your name.");
-                    string tempUserName = Console.ReadLine();
-                    Chart[i, 0] = points.ToString();
-                    Chart[i, 1] = tempUserName;
-                    Skilled = true;
-                    break;
-                }
-            }
-            if (Skilled == false)
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    if (int.Parse(Chart[i, 0]) > worstMoves)
-                    {
-                        worstMovesChartPosition = i;
-                        worstMoves = int.Parse(Chart[i, 0]);
-                    }
-                }
-            }
-            if (points < worstMoves && Skilled == false)
-            {
-                Console.WriteLine("Type in your name.");
-                string tempUserName = Console.ReadLine();
-                Chart[worstMovesChartPosition, 0] = points.ToString();
-                Chart[worstMovesChartPosition, 1] = tempUserName;
-                Skilled = true;
-            }
-            return Skilled;
-        }
-
         static void Main(string[] args)
         {
-            string[,] topFive = new string[5, 2];
             string userInput = string.Empty;
-            byte[,] playingField = GeneratePlayField(5, 10);
-            int userMoves = 0;
+            BalloonsEngine game = new BalloonsEngine(5, 10);
 
-            PrintField(playingField);
+            Console.WriteLine(game.FieldOutput()); 
 
             while (userInput != "exit")
             {
@@ -222,79 +21,72 @@ namespace Balloons_Pops_game
                 switch (userInput)
                 {
                     case "restart":
-                        RestartGame(ref playingField, ref userMoves);
+                        game.RestartGame();
+                        Console.WriteLine(game.FieldOutput()); 
                         break;
 
                     case "top":
-                        sortAndPrintChartFive(topFive); // brake in two distinct parts with output
+                        Console.WriteLine(game.GenerateChart());
                         break;
                     case "exit": 
                         break;
 
                     default:
-                        if (CheckMoveValidity(userInput))
+                        if (game.CheckMoveValidity(userInput))
                         {
-                            int userRow, userColumn;
-                            userRow = ConvertCharToInt(userInput[0]);
-                            userColumn = ConvertCharToInt(userInput[2]);
-
-                            if (userRow > 4 || userRow < 0 || userColumn > 9 || userColumn < 0)
-                            {
-                                Console.WriteLine("Wrong input ! Try Again ! ");
-                                continue;
-                            }
-                            
-
-                            if (PopBalloons(playingField, userRow, userColumn))
-                            {
-                                Console.WriteLine("cannot pop missing ballon!");
-                                continue;
-                            }
-
-                            userMoves++;
-                            if (CollapseRows(playingField))
-                            {
-                                Console.WriteLine("Gratz ! You completed it in {0} moves.", userMoves);
-                                if (signIfSkilled(topFive, userMoves))
-                                {
-                                    sortAndPrintChartFive(topFive);
-                                }
-                                else
-                                {
-                                    Console.WriteLine("I am sorry you are not skillful enough for TopFive chart!");
-                                }
-                                playingField = GeneratePlayField(5, 10);
-                                userMoves = 0;
-                            }
-                            PrintField(playingField);
-                            break;
+                            ProcessMove(userInput, game);
                         }
                         else
                         {
                             Console.WriteLine("Wrong input ! Try Again ! ");
-                            break;
                         }
-
-
+                        break;
                 }
             }
             Console.WriteLine("Good Bye! ");
 
         }
 
-        private static void RestartGame(ref byte[,] playingField, ref int userMoves)
+        private static void ProcessMove(string userInput, BalloonsEngine game)
         {
-            playingField = GeneratePlayField(5, 10);
-            PrintField(playingField);
-            userMoves = 0;
-        }
+            int userRow, userColumn;
+            userRow = ConvertCharToInt(userInput[0]);
+            userColumn = ConvertCharToInt(userInput[2]);
 
-        private static bool CheckMoveValidity(string userInput)
-        {
-            return (userInput.Length == 3) &&
-                 (userInput[0] >= '0' && userInput[0] <= '4') &&
-                 (userInput[2] >= '0' && userInput[2] <= '9') &&
-                 (userInput[1] == ' ' || userInput[1] == '.' || userInput[1] == ',');
+            if (userRow > game.FieldRows || userRow < 0 ||
+                userColumn > game.FieldColumns || userColumn < 0)
+            {
+                Console.WriteLine("Wrong input! Try Again!");
+            }
+
+
+            if (!game.TryPopBalloons(userRow, userColumn))
+            {
+                Console.WriteLine("Cannot pop missing ballon!");
+            }
+
+            game.UserMoves++;
+            game.CollapseRows();
+
+            if (game.CheckIfWinning())
+            {
+                Console.WriteLine(game.FieldOutput());
+                Console.WriteLine("Gratz! You completed it in {0} moves.", game.UserMoves);
+                int place = game.ChartPlaceIndex();
+                if (place != -1)
+                {
+                    Console.WriteLine("Type in your name.");
+                    string username = Console.ReadLine();
+                    game.signIfSkilled(username, place);
+                    Console.WriteLine(game.GenerateChart());
+                }
+                else
+                {
+                    Console.WriteLine("I am sorry you are not skillful enough for TopFive chart!");
+                }
+                game.RestartGame();
+            }
+            Console.WriteLine(game.FieldOutput());
         }
 
         static string GetInput()
