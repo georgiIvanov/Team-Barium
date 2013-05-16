9a65e9685c1712d7eea9236b8f5713f8efb85086
Aneliya Petkova <AneliyaP.Petkova@gmail.com>
2013-05-16 21:29:56 +0300

    BalloonsPop class refactored

--- RefactoredSource/BalloonsPop/BalloonsPop.cs
+++ RefactoredSource/BalloonsPop/BalloonsPop.cs
@@ -2,51 +2,57 @@
 
 namespace Balloons_Pops_game
 {
-    class BalloonsPop
+    public class BalloonsPop
     {
         static void Main(string[] args)
         {
             string userInput = string.Empty;
-            string restartMessage = "Welcome to “Balloons Pops” game. Please try to pop the balloons. Use 'top' to view the top scoreboard, 'restart' to start a new game and 'exit' to quit the game.";
+
             BalloonsEngine game = new BalloonsEngine(5, 10);
 
-            Console.WriteLine(restartMessage);
-            Console.WriteLine(game.FieldOutput()); 
+            PrintIntroMessage();
+            Console.WriteLine(game.FieldOutput());
 
             while (userInput != "exit")
             {
-                Console.WriteLine("Enter a row and column: ");
+                PrintMoveMessage();
                 userInput = GetInput();
 
-                switch (userInput)
-                {
-                    case "restart":
-                        Console.WriteLine();
-                        Console.WriteLine(restartMessage);
-                        game.RestartGame();
-                        Console.WriteLine(game.FieldOutput()); 
-                        break;
-
-                    case "top":
-                        Console.WriteLine(game.GenerateChart());
-                        break;
-                    case "exit": 
-                        break;
-
-                    default:
-                        if (game.CheckMoveValidity(userInput))
-                        {
-                            ProcessMove(userInput, game);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid move or command! Try Again! ");
-                        }
-                        break;
-                }
+                ExecuteCommand(game, userInput);
             }
-            Console.WriteLine("Good Bye! ");
+        }
 
+        private static void ExecuteCommand(BalloonsEngine game, string command)
+        {
+            switch (command)
+            {
+                case "restart":
+                    Console.WriteLine();
+                    PrintIntroMessage();
+                    game.RestartGame();
+                    Console.WriteLine(game.FieldOutput());
+                    break;
+
+                case "top":
+                    Console.WriteLine(game.GenerateChart());
+                    break;
+
+                case "exit":
+                    PrintExitMessage();
+                    break;
+
+                default:
+                    if (game.CheckMoveValidity(command))
+                    {
+                        ProcessMove(command, game);
+                    }
+                    else
+                    {
+                        PrintInvalidCommandMessage();
+                    }
+
+                    break;
+            }
         }
 
         private static void ProcessMove(string userInput, BalloonsEngine game)
@@ -57,7 +63,7 @@ namespace Balloons_Pops_game
 
             if (!game.TryPopBalloons(userRow, userColumn))
             {
-                Console.WriteLine("Cannot pop missing ballon!");
+                PrintIllegalMoveMessage();
             }
 
             game.UserMoves++;
@@ -66,21 +72,25 @@ namespace Balloons_Pops_game
             if (game.CheckIfWinning())
             {
                 Console.WriteLine(game.FieldOutput());
-                Console.WriteLine("Gratz! You completed it in {0} moves.", game.UserMoves);
+                Console.WriteLine("Congratulations! You popped all baloons in {0} moves.", game.UserMoves);
+
                 int place = game.ChartPlaceIndex();
                 if (place != -1)
                 {
-                    Console.WriteLine("Type in your name.");
+                    Console.WriteLine("Please enter your name for the top scoreboard: ");
                     string username = Console.ReadLine();
                     game.RecordHighscore(username, place);
                     Console.WriteLine(game.GenerateChart());
                 }
                 else
                 {
-                    Console.WriteLine("I am sorry you are not skillful enough for TopFive chart!");
+                    Console.WriteLine("Game Over!");
+                    Console.WriteLine(game.GenerateChart());
                 }
+
                 game.RestartGame();
             }
+
             Console.WriteLine(game.FieldOutput());
         }
 
@@ -92,6 +102,37 @@ namespace Balloons_Pops_game
         static int ConvertCharToInt(char input)
         {
             return input - '0';
+        }
+
+        public static void PrintIntroMessage()
+        {
+            Console.WriteLine(
+                "Welcome to “Balloons Pops” game. Please try to pop the balloons. "
+                + "Use 'top' to view the top scoreboard, 'restart' to start a new game and 'exit' to quit the game.");
+            Console.WriteLine();
+        }
+
+        private static void PrintIllegalMoveMessage()
+        {
+            Console.WriteLine("Illegal move: cannot pop missing ballon! Try Again!");
+            Console.WriteLine();
+        }
+
+        private static void PrintMoveMessage()
+        {
+            Console.Write("Enter a row and column: ");
+        }
+
+        private static void PrintInvalidCommandMessage()
+        {
+            Console.WriteLine("Invalid move or command! Try Again!");
+            Console.WriteLine();
+        }
+
+        private static void PrintExitMessage()
+        {
+            Console.WriteLine("So sorry you are leaving! Good bye!");
+            Console.WriteLine();
         }
     }
 }
