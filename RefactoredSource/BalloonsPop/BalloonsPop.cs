00d07b154edad2e0c3460c542b487a3bfa1e80eb
Aneliya Petkova <AneliyaP.Petkova@gmail.com>
2013-05-16 22:44:49 +0300

    Added xml comments to the BalloonsPop class

--- RefactoredSource/BalloonsPop/BalloonsPop.cs
+++ RefactoredSource/BalloonsPop/BalloonsPop.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿// ********************************
+// <copyright file="BalloonsPop.cs" company="Telerik Academy">
+// Copyright (c) 2013 Telerik Academy. All rights reserved.
+// </copyright>
+//
+// ********************************
+using System;
 
 namespace Balloons_Pops_game
 {
+    /// <summary>
+    /// The main class of the console game Ballons-Pop.
+    /// </summary>
     public class BalloonsPop
     {
+        /// <summary>
+        /// The entry point of the program.
+        /// </summary>
         static void Main(string[] args)
         {
             string userInput = string.Empty;
@@ -22,6 +34,11 @@ namespace Balloons_Pops_game
             }
         }
 
+        /// <summary>
+        /// Executes a command from a predefined set of commands or breaks if invalid command.
+        /// </summary>
+        /// <param name="game">Instace of the BalloonsEngine class.</param>
+        /// <param name="command">User input command to execute.</param>
         private static void ExecuteCommand(BalloonsEngine game, string command)
         {
             switch (command)
@@ -55,6 +72,11 @@ namespace Balloons_Pops_game
             }
         }
 
+        /// <summary>
+        /// Processes the move entered by the player.
+        /// </summary>
+        /// <param name="userInput">Row and column of the move.</param>
+        /// <param name="game">Instace of the BalloonsEngine class.</param>
         private static void ProcessMove(string userInput, BalloonsEngine game)
         {
             int userRow, userColumn;
@@ -94,16 +116,28 @@ namespace Balloons_Pops_game
             Console.WriteLine(game.FieldOutput());
         }
 
+        /// <summary>
+        /// Reads input data from the console.
+        /// </summary>
+        /// <returns>Trimmed input data to lowercase.</returns>
         static string GetInput()
         {
             return Console.ReadLine().ToLower().Trim();
         }
 
+        /// <summary>
+        /// Convert char character to integer number.
+        /// </summary>
+        /// <param name="input">The character.</param>
+        /// <returns>Integer number;</returns>
         static int ConvertCharToInt(char input)
         {
             return input - '0';
         }
 
+        /// <summary>
+        /// Prints intro message to the console.
+        /// </summary>
         public static void PrintIntroMessage()
         {
             Console.WriteLine(
@@ -112,27 +146,39 @@ namespace Balloons_Pops_game
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Prints illegal move message to the console.
+        /// </summary>
         private static void PrintIllegalMoveMessage()
         {
             Console.WriteLine("Illegal move: cannot pop missing ballon! Try Again!");
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Prints message to the console for entering row and column of the move.
+        /// </summary>
         private static void PrintMoveMessage()
         {
             Console.Write("Enter a row and column: ");
         }
 
+        /// <summary>
+        /// Print message for invalid command to the console.
+        /// </summary>
         private static void PrintInvalidCommandMessage()
         {
             Console.WriteLine("Invalid move or command! Try Again!");
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Print exit message and info for Ballons-Pop game to the console.
+        /// </summary>
         private static void PrintExitMessage()
         {
-            Console.WriteLine("So sorry you are leaving! Good bye!");
-            Console.WriteLine();
+            Console.WriteLine("\nSo sorry you are leaving! Good bye!\n");
+            Console.WriteLine("Balloons-Pop-4 game\nVersion 1.0\n" + "Copyright (c) 2013 Telerik Academy. All rights reserved.\n");
         }
     }
 }
